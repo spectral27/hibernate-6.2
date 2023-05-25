6182afc847f681361de9f3d3c217043fc92475de
spectre2727 <gsoldano27@outlook.com>
2023-05-26 00:41:14 +0200

    hibernate

---
+++ benchmark-jpa-gradle/src/main/resources/init.sql
@@ -0,0 +1,12 @@
+create table Processor(
+    id integer,
+    model varchar(255),
+    cores integer,
+    threads integer,
+    maxfrequency varchar(255),
+    cache varchar(255),
+    maxram varchar(255),
+    ramtype varchar(255),
+    ramchannels integer,
+    primary key (id)
+);
